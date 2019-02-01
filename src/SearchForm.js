17feb710c2d019fafc6c66e170da8d1d8149a01d
Themis Chatzis <chatzis.themis@gmail.com>
2019-02-01 15:47:07 +0000

    Search form added

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 
 class SearchForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ""
+        };
+
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    searchGiphy(event) {
+        event.preventDefault();
+    }
+
+    handleChange(e) {
+        this.setState({
+            value: e.target.value
+        });
+    }
+
     render() {
       return (
         <div>
-          <h3>form</h3>
+          <h3>Search</h3>
+
+          <form onSubmit={this.searchGiphy}>
+            <input
+                type="text"
+                value={this.state.value}
+                onChange={this.handleChange}
+                placeholder="Grand Manolo"
+            />
+          </form>
         </div>
       )
     }
