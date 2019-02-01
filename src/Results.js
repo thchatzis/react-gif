72fcd657c5f7c9499439346e172e277b80099e7c
Themis Chatzis <chatzis.themis@gmail.com>
2019-02-01 18:34:54 +0000

    Displaying results

--- src/Results.js
+++ src/Results.js
@@ -3,11 +3,21 @@ import React, { Component } from 'react';
 class Results  extends Component {
     render() {
         console.log(this.props.gifs);
-      return (
-        <div>
-          <h3>results</h3>
-        </div>
-      )  }
-  }
+        return (
+            <div>
+                <h3>results</h3>
+                {this.props.gifs.map(gif => {
+                    return (
+                        <img 
+                        key={gif.id}
+                        src={gif.images.preview_gif.url}
+                        alt={gif.title}
+                         />
+                    );
+                })}
+            </div>
+        );
+    }
+}
 
 export default Results;
